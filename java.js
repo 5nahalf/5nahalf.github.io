4a49026bb6ad6913fb8d647bf89197e851ddc507
5nahalf <5nahalf@comcast.net>
2015-03-23 10:47:52 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 var myAge = 30
-
+function nextYear (myAge)
 {
-	console.log ("Right now I am " + myAge + " years old.")
+	console.log ("Right now I am " + myAge + " years old.");
+	myAge++;
+	console.log ("Next year I will be" + (myAge++) + "years old");
 	
 }
