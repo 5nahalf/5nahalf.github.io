4b88988ccca9c218b51c30f5c06163f1d035cc0d
5nahalf <5nahalf@comcast.net>
2015-03-23 10:55:46 -0500

    Update java.js

--- java.js
+++ java.js
@@ -3,6 +3,6 @@ function getAge (myAge)
 	console.log ("Right now I am " + myAge + " years old.");
 	myAge++;
 	console.log ("Next year I will be" + myAge + "years old");
-	
+	alert ("I think its working?");
 }
-alert (I think its working?);
+
